1452c2a6076ed5417d3807ef3a5d2e73b03a2205
c14yi <student@c02g20thddr0.dalton.org>
2012-11-26 09:44:23 -0500

    Queue Test for Yuri and Jetta's queue code
    Size, GetAll, Clear

--- IntQueueTest.java
+++ IntQueueTest.java
@@ -1,7 +1,18 @@
 class IntQueueTest {
     public static void main(String[] args) {
 	IntQueue iq = new IntQueue(5);
+        iq[0] = 7;
+        iq[1] = 4;
+        iq[2] = 5;
+        iq[3] = 1;
+        iq[4] = 9;
 
 	//tests here:
+        //clear, getall, size
+        //yuri and jetta
+        
+        System.out.println(iq.size);
+        System.out.println(iq.GetAll);
+	System.out.println(iq.clear);   
     }
 }
